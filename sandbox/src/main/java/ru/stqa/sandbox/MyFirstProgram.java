655cf8fdab2a2899927fb75c866b7b735b56a5da
NXISUS <valeriycherkashin@yahoo.com>
2016-04-14 13:23:23 -0500

    Added some functions

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -1,7 +1,29 @@
 package ru.stqa.sandbox;
 
 public class MyFirstProgram {
-public static void main(String[] args){
-System.out.println("Hello word! And my family!");
+  public static void main(String[] args) {
+    hello("world");
+    hello ("Valeriy");
+
+    double l =5;
+
+    System.out.println ("Площадь квадрата со стороной " +l+ "="+area(l) );
+
+    double a =4;
+    double b =6;
+    System.out.println ("Площадь прямоугольника со сторонами " +a+ " и " +b+ " = " + area(a, b));
+  }
+
+  private static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody +"!");
+  }
+
+public static double area (double len) {
+  return len * len;
+
 }
+  public static double area (double a, double b)
+  {return a * b;}
+
 }
