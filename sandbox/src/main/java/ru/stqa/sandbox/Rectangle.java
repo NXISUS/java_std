9085a92761a0ea9279c0f4390cff5d8921bc8b9a
NXISUS <valeriycherkashin@yahoo.com>
2016-04-14 14:41:03 -0500

    Добавлен конструктор

---
+++ sandbox/src/main/java/ru/stqa/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.sandbox;
+
+/**
+ * Created by Evolve Computing on 4/14/2016.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.sandbox;
+
+/**
+ * Created by Evolve Computing on 4/14/2016.
+ */
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
