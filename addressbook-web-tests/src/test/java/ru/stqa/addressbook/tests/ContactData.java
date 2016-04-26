44bbd91aca4639e6dc67916d959339e0fdc26c6f
NXISUS <valeriycherkashin@yahoo.com>
2016-04-26 14:37:53 -0500

    Created object ContactData

---
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactData.java
@@ -0,0 +1,67 @@
+package ru.stqa.addressbook.tests;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String home;
+  private final String mobilephone;
+  private final String workphone;
+
+  public ContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String home, String mobilephone, String workphone) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.home = home;
+    this.mobilephone = mobilephone;
+    this.workphone = workphone;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getWorkphone() {
+    return workphone;
+  }
+}
